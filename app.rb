fe1fdc287f1a555f4f0fab3acf78bc04ced64f0f
DB Christenson <dbeauchristenson@gmail.com>
2024-04-16 21:08:40 +0000

    almost done

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 
-get("/") do
+def get_codes
   erk = ENV.fetch("EXCHANGE_RATE_KEY")
   api_url = "https://api.exchangerate.host/list?access_key=#{erk}"
 
@@ -16,14 +16,34 @@ get("/") do
   parsed_data = JSON.parse(raw_data_string)
 
   currencies_hash = parsed_data.fetch("currencies")
-  @currency_codes = currencies_hash.keys
+  currency_codes = currencies_hash.keys
+
+  return currency_codes
+end
+
+get("/") do
+  @currency_codes = get_codes()
 
   erb :index
 end
 
 get("/:currency1") do |currency1|
   @selected_currency1 = currency1
+  @currency_codes = get_codes()
+
+  erb :select_currency2
 end
 
 get("/:currency1/:currency2") do |currency1, currency2|
+  @selected_currency1 = currency1
+  @selected_currency2 = currency2
+
+  # Logic to retrieve the exchange rate between currency1 and currency2
+  access_key = ENV['EXCHANGE_RATE_KEY']
+  api_url = "https://api.exchangerate.host/convert?access_key=#{access_key}&from=#{currency1}&to=#{currency2}"
+  
+  response = HTTP.get(api_url)
+  result = response.parse(:json)
+
+  erb :show_rate
 end
